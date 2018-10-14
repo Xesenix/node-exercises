3190f3798fdcfc79e236b18485b2b3ba5c705cb2
Paweł Kapalla <pkapalla@xesenix.pl>
2018-10-14 17:49:18 +0200

    NODE-01: feat(stream): create transform stream for tracking progress

--- stream/gzip.js
+++ stream/gzip.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
 const zlib = require('zlib');
 const file = process.argv[2];
+const { Transform } = require('stream');
+// Transform is duplex stream that handles write and read actions inside transform method
+const progress = new Transform({
+    transform(chunk, encoding, cb) {
+        process.stdout.write('.');
+        cb(null, chunk);
+    }
+})
 
 fs.createReadStream(file)
     .pipe(zlib.createGzip())
-    .pipe(fs.createWriteStream(`${file}.gz`));
+    // alternative progress
+    // .on('data', () => process.stdout.write('.'))
+    .pipe(progress)
+    .pipe(fs.createWriteStream(`${file}.gz`))
+    .on('finish', () => process.stdout.write('Done\n'));
